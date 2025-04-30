3bc8642c056784ea12792723720448f993992804
iandemeloely <iandemeloely@gmail.com>
2025-04-30 11:54:35 -0300

    pi

--- App/Login/login.php
+++ App/Login/login.php
@@ -3,17 +3,17 @@ session_start();
 require 'config.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['nomeuser'];
-    $password = $_POST['password'];
+    $nomeuser = $_POST['nomeuser'];
+    $password = $_POST['senha'];
 
     // Consulta ao banco de dados para verificar o usuário
     $stmt = $pdo->prepare("SELECT * FROM user WHERE nomeuser = ?");
-    $stmt->execute([$username]);
+    $stmt->execute([$nomeuser]);
     $user = $stmt->fetch();
 
-    if ($user && password_verify($password, $user['password'])) {
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['username'] = $user['username'];
+    if ($user && password_verify($senha, $user['senha'])) {
+        $_SESSION['iduser'] = $user['id'];
+        $_SESSION['nomeuser'] = $user['nomeuser'];
         header('Location: itens.php');
         exit();
     } else {
@@ -21,3 +21,148 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login/Cadastro</title>
+    <link rel="stylesheet" href="../View/CSS/login.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+
+    <!-- credits to the writter @leonam-silva-de-souza -->
+    <div class="container">
+        <div class="form-box login">
+            <form action="#">
+                <h1>Login</h1>
+                <div class="input-box">
+                    <input type="text" placeholder="Username" required>
+                    <i class='bx bxs-user'></i>
+                </div>
+                <div class="input-box">
+                    <input type="password" placeholder="Password" required>
+                    <i class='bx bxs-lock-alt'></i>
+                </div>
+                <div class="forgot-link">
+                    <a href="#">Esqueci a senha</a>
+                </div>
+                <button type="submit" class="btn">Login</button>
+                <p>ou faça login com plataformas sociais</p>
+                <div class="social-icons">
+                    <a href="#"><i class='bx bxl-google'></i></a>
+                    <a href="#"><i class='bx bxl-facebook'></i></a>
+                </div>
+            </form>
+        </div>
+
+        <div class="form-box register">
+            <form action="../App/Controller/processar-cadastro.php" method="POST">
+                <h1>Cadastro</h1>
+        
+                <div class="input-box">
+                    <input type="text" name="nomeuser" placeholder="Nome Completo" required>
+                    <i class='bx bxs-user'></i>
+                </div>
+        
+                <div class="input-box">
+                    <input type="text" name="cpf" placeholder="CPF" required>
+                    <i class='bx bxs-id-card'></i>
+                </div>
+        
+                <div class="input-box">
+                    <input type="email" name="email" placeholder="E-mail" required>
+                    <i class='bx bxs-envelope'></i>
+                </div>
+        
+                <div class="input-box">
+                    <input type="text" name="telefone" placeholder="Telefone" required>
+                    <i class='bx bxs-phone'></i>
+                </div>
+        
+                <div class="input-box">
+                    <input type="date" name="dataNascimento" required>
+                    <i class='bx bxs-calendar'></i>
+                </div>
+        
+                <div class="input-box">
+                    <select name="genero" required>
+                        <option value="" disabled selected>Selecione o Gênero</option>
+                        <option value="Masculino">Masculino</option>
+                        <option value="Feminino">Feminino</option>
+                        <option value="Outro">Outro</option>
+                    </select>
+                    <i class='bx bxs-user-circle'></i>
+                </div>
+        
+                <div class="input-box">
+                    <input type="text" name="endereco" placeholder="Endereço" required>
+                    <i class='bx bxs-home'></i>
+                </div>
+        
+                <div class="input-box">
+                    <input type="text" name="cidade" placeholder="Cidade" required>
+                    <i class='bx bxs-city'></i>
+                </div>
+        
+                <div class="input-box">
+                    <input type="text" name="estado" placeholder="Estado" required>
+                    <i class='bx bxs-map'></i>
+                </div>
+        
+                <div class="input-box">
+                    <input type="text" name="cep" placeholder="CEP" required>
+                    <i class='bx bxs-map-pin'></i>
+                </div>
+
+                <div class="input-box">
+                    <input type="text" name="info_medica" placeholder="Informações medicas" required>
+                    <i class='bx bxs-map-pin'></i>
+                </div>
+        
+                <div class="input-box">
+                    <input type="password" name="senha" placeholder="Senha" required>
+                    <i class='bx bxs-lock-alt'></i>
+                </div>
+        
+        
+                <button type="submit" class="btn">Cadastrar</button>
+                <p>ou cadastre-se pelas plataformas sociais</p>
+                
+                <div class="social-icons">
+                    <a href="#"><i class='bx bxl-google'></i></a>
+                    <a href="#"><i class='bx bxl-facebook'></i></a>
+                </div>
+            </form>
+        </div>
+
+        <div class="toggle-box">
+            <div href="index.html" class="toggle-panel toggle-left">
+                <a href="./index.html">
+                    <img class="logo" src="../View/Imagens/Logo.png" alt="Logo">
+                </a>
+                <h1>Olá, bem-vindo!</h1>
+                <p>Não tem uma conta?</p>
+                <button class="btn register-btn">Cadastrar</button>
+            </div>
+
+            <div class="toggle-panel toggle-right">
+                <a href="./index.html">
+                    <img class="logo" src="../View/Imagens/Logo.png" alt="Logo">
+                </a>
+                <h1>Bem vindo de volta!</h1>
+                <p>Já tem uma conta?</p>
+                <button class="btn login-btn">Login</button>
+            </div>
+        </div>
+    </div>
+
+    <script src="../View/JavaScript/loggin.js"></script>
+        
+</body>
+
+</html>
